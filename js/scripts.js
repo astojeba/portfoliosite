b0c7b25f3b9fcd9a04acce720f441e6d3dcfca75
Staged Files <astojeba@gmail.com>
2018-10-27 13:25:27 +0200

    change

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,31 @@
 console.log("Hi, welcome to my portfolio site!\n\nDon't hesitate to get in touch with me should you be looking for someone to collaborate with for UX-related projects!")
+
+$(document).ready(function() {
+
+  var scrollLink = $('.scroll');
+
+  // Smooth scrolling
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000 );
+  });
+
+  // Active link switching
+  $(window).scroll(function() {
+    var scrollbarLocation = $(this).scrollTop();
+
+    scrollLink.each(function() {
+
+      var sectionOffset = $(this.hash).offset().top - 20;
+
+      if ( sectionOffset <= scrollbarLocation ) {
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+      }
+    })
+
+  })
+
+})
